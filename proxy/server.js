9bbe5bcaa1c9731ff0a7d3c68ddbfeead1341a83
Ike Okonkwo <paschal83@gmail.com>
2019-04-12 01:18:51 -0700

    Upadred all proxy files

--- proxy/server.js
+++ proxy/server.js
@@ -11,33 +11,33 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 // app.use(
-//   '/:restaurantId',
+//   '/:id',
 //   proxy({ target: 'http://localhost:3001/', changeOrigin: true })
 // );
-app.use(
-  '/ratings_ambience/:id',
-  proxy({ target: 'http://localhost:3002/', changeOrigin: true })
-);
-app.use(
-  '/reviews/:id',
-  proxy({ target: 'http://localhost:3002/', changeOrigin: true })
-);
-app.use(
-  '/restaurants/:id',
-  proxy({ target: 'http://localhost:3003/', changeOrigin: true })
-);
-app.use(
-  '/menus/:id',
-  proxy({ target: 'http://localhost:3004/', changeOrigin: true })
-);
+// app.use(
+//   '/ratings_ambience/:id',
+//   proxy({ target: 'http://localhost:3002/', changeOrigin: true })
+// );
+// app.use(
+//   '/reviews/:id',
+//   proxy({ target: 'http://localhost:3002/', changeOrigin: true })
+// );
+// app.use(
+//   '/restaurants/:id',
+//   proxy({ target: 'http://localhost:3003/', changeOrigin: true })
+// );
+// app.use(
+//   '/menus/:id',
+//   proxy({ target: 'http://localhost:3004/', changeOrigin: true })
+// );
 
 app.listen(port, () => {
   console.log(`server running at: :${port}`);
